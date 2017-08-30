77c6f18f26b50eadd716d7f5ee7dd19127048e1c
Devin Roche <Roche.devin@ymail.com>
2017-08-29 20:47:50 -0700

    get all users endpoint and get active hosts

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const MongoClient = require('mongodb').MongoClient
 const app = express()
 
 var helpers = require("./serverHelper");
+var database
 MongoClient.connect(
   "***REMOVED***",
   (err, database) => {
-    if (err) {
-      return console.log(err);
-    }
-    db = database;
+    if (err) 
+      return err;
+    
     console.log("Connected to database")
-    app.listen(3000, ()=>{
-        console.log('server 3000')
+    app.listen(3000, () => {
+        console.log('localhost 3000')
     })
-    require("./routes.js")(app, db, app)
+    console.log(database)
+    require("./routes.js")(app, database, app)
   }
 );
+
