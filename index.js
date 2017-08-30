cbb040a06d7eed068b1671a5b8d4482f14d3c03b
devin <roche.devin@ymail.com>
2017-08-30 15:09:49 -0700

    adding users from request

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
 const app = express()
 
 var helpers = require("./serverHelper");
 var database
+app.use(bodyParser.json());
+
 MongoClient.connect(
   "mongodb://admin:password@ds044689.mlab.com:44689/sparespacedb",
   (err, database) => {
     if (err) 
       return err;
     
-    app.listen(3000, () => {
+    app.listen(3000, () => 
         console.log('localhost 3000')
-    })
+    )
 
-    require("./routes.js")(app, database, helpers)
+    require("./routes.js")(app, database, helpers, bodyParser);
   }
 );
 
