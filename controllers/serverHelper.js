f81d7aae11a42ade8fe009bb699a0659bc7d7034
Devin Roche <Roche.devin@ymail.com>
2017-08-31 20:59:11 -0700

    partial commit

---
+++ controllers/serverHelper.js
@@ -0,0 +1,75 @@
+// var self = module.exports = {
+//     addUsertoDB(user, db){
+//         db.insert(user)
+//     },
+//     checkUsers(userObj, db, res){
+//         db.findOne({ username: userObj.username }, function(err, user) {
+//             if (user) {
+//                 console.log("user already exists");
+//             } else {
+//                 var objUser = userData(userObj)
+//                 self.addUsertoDB(objUser, db);
+//             }
+//         })
+//     },
+//     pruneHost(userObj){
+//         var hostArr = []
+//         for(var i=0; i<userObj.length; i++){
+//             hostObj = {
+//                 fullname: userObj[i].fullname,
+//                 username: userObj[i].username,
+//                 contact: userObj[i].contact,
+//                 space: userObj[i].space
+//             }
+//             hostArr.push(hostObj)
+//         }
+//         return hostArr
+//     }
+// }
+
+// userData = function(user){
+//     var userObj = {
+//         username: user.username,
+//         fullname: user.fullname,
+//         contact: {
+//             email: user.contact.email,
+//             phone: user.contact.phone,
+//         },
+//         isHost: false,
+//         isStoring: false,
+//         landlords: [],
+//         renters: [],
+//         space: {
+//             size: user.space.size
+//         }
+//     }
+//     console.log(userObj)
+//     return userObj
+// }
+
+const mongoose = require('mongoose')
+    User = mongoose.model('Users')
+    
+
+
+module.exports = {
+    allusers(req, res) {
+        User.find({}, function (err, users) {
+            console.log(users)
+            if (err)
+                res.send(err);
+
+            res.send(users)
+        })
+    },
+    createUser(req,res) {
+        var newUser = new User(req.body)
+        console.log(req.body)
+        newUser.save(function (err, user) {
+            if(err)
+                res.send(err)
+
+            res.send(user)
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const express = require('express')
-const MongoClient = require('mongodb').MongoClient
-const bodyParser = require('body-parser')
-const app = express()
+  MongoClient = require('mongodb').MongoClient
+  bodyParser = require('body-parser')
+  mongoose = require('mongoose')
+  User = require("./models/userModel")
+  port = process.env.PORT || 3001,
+  app = express()
 
-var helpers = require("./serverHelper");
-var database
+var helpers = require("./controllers/serverHelper");
+  routes = require('./routes/routes')
+
+routes(app)
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(
-  "mongodb://admin:password@ds044689.mlab.com:44689/sparespacedb",
-  (err, database) => {
-    if (err) 
-      return err;
+app.listen(port)
+
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://admin:password@ds044689.mlab.com:44689/sparespacedb")
+
+require("./routes/routes.js")(app, bodyParser)
+
+// MongoClient.connect(
+//   "mongodb://admin:password@ds044689.mlab.com:44689/sparespacedb",
+//   (err, database) => {
+//     if (err) 
+//       return err;
     
-    app.listen(3001, () => 
-        console.log('localhost 3001')
-    )
+//     app.listen(3001, () => 
+//         console.log('localhost 3001')
+//     )
 
-    require("./routes.js")(app, database, helpers, bodyParser);
-  }
-);
+//     require("./routes/routes.js")(app, database, bodyParser);
+//   }
+// );
 
