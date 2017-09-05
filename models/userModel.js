a4a4b493d47a07fd894158d6716844a5897f965f
devin <roche.devin@ymail.com>
2017-09-05 14:21:47 -0700

    change renter schema

--- models/userModel.js
+++ models/userModel.js
@@ -2,20 +2,24 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema 
 
-var userSchema = new Schema ({
+var renter = new Schema ({
     username: {type:String, required: true, unique: true}, 
     fullname: { type: String, required: true }, 
     contact: {
         email: { type: String, required: true },
         phone: { type: String, required: true }, 
     },
-    isHost: { type: Boolean, default: false },
     isStoring: { type: Boolean, default: false },
-    landlords: [],
-    renters: [],
-    space: {
-        size: String
-    }
+    landlord: {
+        fullname: {type: String},
+        contact: {
+            email: { type: String, required: true },
+            phone: { type: String, required: true }, 
+        },
+        location: {type: String},
+        price: {type: String},
+        duration: {type: String}
+    },
 })
 
 userSchema.plugin(uniqueValidator)
