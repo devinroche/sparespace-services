c10e212765025ba8ebe790062e191f31e020ed22
Devin Roche <Roche.devin@ymail.com>
2017-09-17 19:57:27 -0700

    init new schemas

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema 
 
-var renter = new Schema ({
+var userSchema = new Schema ({
     username: {type:String, required: true, unique: true}, 
     fullname: { type: String, required: true }, 
     contact: {
@@ -19,7 +19,49 @@ var renter = new Schema ({
         location: {type: String},
         price: {type: String},
         duration: {type: String}
+    }
+})
+
+var renterSchema = new Schema({
+    username: {type: String, required: true, unique:true},
+    fullname: { type: String, required: true }, 
+    contact: {
+        email: { type: String, required: true },
+        phone: { type: String, required: true }, 
     },
+    isStoring: { type: Boolean, default: false },
+    host: {
+        fullname: {type: String},
+        contact: {
+            email: { type: String},
+            phone: { type: String}, 
+        },
+        location: {type: String},
+        price: {type: String},
+        startDate: {type: Date},
+        endDate: {type: Date}
+    }
+})
+
+var hostSchema = new Schema({
+    username: {type: String, required: true, unique:true},
+    fullname: { type: String, required: true }, 
+    contact: {
+        email: { type: String, required: true },
+        phone: { type: String, required: true }, 
+    },
+    isHosting: { type: Boolean, default: false },
+    renters: {
+        fullname: {type: String},
+        contact: {
+            email: { type: String},
+            phone: { type: String}, 
+        },
+        location: {type: String},
+        price: {type: String},
+        startDate: {type: Date},
+        endDate: {type: Date}
+    }
 })
 
 userSchema.plugin(uniqueValidator)
