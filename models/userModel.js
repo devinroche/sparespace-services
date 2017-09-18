05a620074108d91543716209e8d29ac4ae061b65
Devin Roche <Roche.devin@ymail.com>
2017-09-17 20:01:42 -0700

    comment schemas out

--- models/userModel.js
+++ models/userModel.js
@@ -22,47 +22,47 @@ var userSchema = new Schema ({
     }
 })
 
-var renterSchema = new Schema({
-    username: {type: String, required: true, unique:true},
-    fullname: { type: String, required: true }, 
-    contact: {
-        email: { type: String, required: true },
-        phone: { type: String, required: true }, 
-    },
-    isStoring: { type: Boolean, default: false },
-    host: {
-        fullname: {type: String},
-        contact: {
-            email: { type: String},
-            phone: { type: String}, 
-        },
-        location: {type: String},
-        price: {type: String},
-        startDate: {type: Date},
-        endDate: {type: Date}
-    }
-})
+// var renterSchema = new Schema({
+//     username: {type: String, required: true, unique:true},
+//     fullname: { type: String, required: true }, 
+//     contact: {
+//         email: { type: String, required: true },
+//         phone: { type: String, required: true }, 
+//     },
+//     isStoring: { type: Boolean, default: false },
+//     host: {
+//         fullname: {type: String},
+//         contact: {
+//             email: { type: String},
+//             phone: { type: String}, 
+//         },
+//         location: {type: String},
+//         price: {type: String},
+//         startDate: {type: Date},
+//         endDate: {type: Date}
+//     }
+// })
 
-var hostSchema = new Schema({
-    username: {type: String, required: true, unique:true},
-    fullname: { type: String, required: true }, 
-    contact: {
-        email: { type: String, required: true },
-        phone: { type: String, required: true }, 
-    },
-    isHosting: { type: Boolean, default: false },
-    renters: {
-        fullname: {type: String},
-        contact: {
-            email: { type: String},
-            phone: { type: String}, 
-        },
-        location: {type: String},
-        price: {type: String},
-        startDate: {type: Date},
-        endDate: {type: Date}
-    }
-})
+// var hostSchema = new Schema({
+//     username: {type: String, required: true, unique:true},
+//     fullname: { type: String, required: true }, 
+//     contact: {
+//         email: { type: String, required: true },
+//         phone: { type: String, required: true }, 
+//     },
+//     isHosting: { type: Boolean, default: false },
+//     renters: {
+//         fullname: {type: String},
+//         contact: {
+//             email: { type: String},
+//             phone: { type: String}, 
+//         },
+//         location: {type: String},
+//         price: {type: String},
+//         startDate: {type: Date},
+//         endDate: {type: Date}
+//     }
+// })
 
 userSchema.plugin(uniqueValidator)
 module.exports = mongoose.model('Users', userSchema)
