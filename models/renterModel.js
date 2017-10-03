27276480772049b5719942136f437980f1f88c06
devin <roche.devin@ymail.com>
2017-10-03 16:06:00 -0700

    clean code login renter

---
+++ models/renterModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
+const Schema = mongoose.Schema 
+
+var renter = new Schema ({
+    username: {type:String, required: true, unique: true}, 
+    fullname: { type: String, required: true }, 
+    password: { type: String, required: true }, 
+    contact: {
+        email: { type: String, required: true },
+        phone: { type: String, required: true }, 
+    },
+    isStoring: { type: Boolean, default: false },
+    landlord: {
+        fullname: {type: String},
+        contact: {
+            email: { type: String},
+            phone: { type: String}, 
+        },
+        location: {type: String},
+        price: {type: String},
+        duration: {type: String}
+    }
+}, { collection: 'renterinfo' })
+
+renter.plugin(uniqueValidator)
+module.exports = mongoose.model('Renter', renter)
+
+
+/* post test dummy user
+{
+    username: "janedoe123",
+    fullname: "Jane Doe",
+    contact: {
+        email: "janedoe@email.com",
+        phone: "123-456-7890",
+    }
+}
+*/
