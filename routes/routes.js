5ddb9a4ecef8a8a7ce636691f2324b894f53e5ae
devin <roche.devin@ymail.com>
2017-10-04 15:00:30 -0700

    remove / from route

--- routes/routes.js
+++ routes/routes.js
@@ -10,6 +10,6 @@ module.exports = function(app) {
         .put(helpers.updateUser)
         .delete(helpers.deleteUser)
 
-    app.route('/login/')
+    app.route('/login')
         .post(helpers.loginUser)
 }
