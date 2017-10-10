2c3b5ffd1be71d7f22e0aabe3412229391eaa906
devin <roche.devin@ymail.com>
2017-10-10 10:01:52 -0700

    fix bugs from merge conflict

--- routes/routes.js
+++ routes/routes.js
@@ -11,6 +11,8 @@ module.exports = function(app) {
         .delete(helpers.deleteUser)
 
     app.route('/login')
+        .post(helpers.loginUser)
+
     app.route('/verify/:id')
     	.get(helpers.verify_user)
         .post(helpers.loginUser)
