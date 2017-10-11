02ee8ec35a3533557e75c25c4a700189ee46a691
devin <roche.devin@ymail.com>
2017-10-11 16:19:31 -0700

    env file changes

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ const routes = require('./routes/routes')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressValidator()); // Add this after the bodyParser middlewares!
+app.use(expressValidator()); 
 app.use(cors())
 
 routes(app);
 app.listen(port)
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb://admin:password@ds044689.mlab.com:44689/sparespacedb")
+mongoose.connect(process.env.DB_CONNECT)
 var db = mongoose.connection;
 
 require("./routes/routes.js")(app, bodyParser)
