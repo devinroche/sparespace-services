1002fda8918fa1926860f713f1f569bdb924fe7e
devin <roche.devin@ymail.com>
2017-10-31 11:00:17 -0700

    fix codacy errors

--- controllers/userHelper.js
+++ controllers/userHelper.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
-const mailHelper = require("./mailHelper")
-const User = mongoose.model("User")
+const mailHelper = require("./mailHelper");
+const User = mongoose.model("User");
 const googleMapsClient = require("@google/maps");
 
 
 module.exports = {
     allUsers(req, res){
         User.find({}, function (err, user) {
-            if (err)
+            if (err){
                 res.json(err);
+            }
 
             res.send(user);
         })
@@ -16,8 +17,9 @@ module.exports = {
     createUser(req, res) {
         var newUser = new User(req.body)
         newUser.save(function (err, user) {
-            if(err)
+            if(err){
                 res.json(err);
+            }
 
             mailHelper.sendEmailVerify(req.body.contact.email);
             res.send(user);
@@ -26,33 +28,37 @@ module.exports = {
 
     getUser(req, res){
         User.findById(req.params.id, function(err, user){
-            if(err)
+            if(err){
                 res.json(err);
+            }
 
             res.send(user);
-        })
+        });
     },
 
     updateUser(req, res){
         User.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, user){
-            if (err)
+            if (err){
                 res.json(err);
+            }
 
             res.json({ message: "user updated", user });
-        })
+        });
     },
     deleteUser(req, res){
         User.remove({_id: req.params.id}, function(err, user){
-            if(err)
+            if(err){
                 res.json(err);
+            }
 
             res.json({user});
-        })
+        });
     },
     loginUser(req, res){
         User.find({"contact.email": req.body.email, password: req.body.password}, function (err, user) {
-            if (err)
+            if (err){
                 res.json(err);
+            }
 
             if(user.length === 0){
                 res.sendStatus(404);
@@ -60,14 +66,15 @@ module.exports = {
             else{
                 res.send(user);
             }
-        })
+        });
     },
 
     verifyUser(req,res) {
         User.findOneAndUpdate({"contact.email": req.params.email}, {isVerified: true}, function(err, user){
-            if (err)
-                res.json(err)
-                
+            if (err){
+                res.json(err);
+            }
+
             res.send("account verified");
         })
     },
