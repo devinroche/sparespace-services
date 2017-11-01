410306cfa27b854876c5b94fdefc2e9de9dc2253
devin <roche.devin@ymail.com>
2017-10-31 22:43:38 -0700

    add travisci

--- controllers/userHelper.js
+++ controllers/userHelper.js
@@ -1,99 +1,107 @@
-const mongoose = require("mongoose");
-const mailHelper = require("./mailHelper");
-const User = mongoose.model("User");
-const googleMapsClient = require("@google/maps");
+const mongoose = require('mongoose');
+const mailHelper = require('./mailHelper');
 
+const User = mongoose.model('User');
+const googleMapsClient = require('@google/maps');
 
 module.exports = {
-    allUsers(req, res){
-        User.find({}, function (err, user) {
-            if (err){
-                res.json(err);
-            }
+	allUsers(req, res) {
+		User.find({}, (err, user) => {
+			if (err) 
+				res.json(err);
+      
 
-            res.send(user);
-        });
-    },
-    createUser(req, res) {
-        var newUser = new User(req.body);
-        newUser.save(function (err, user) {
-            if(err){
-                res.json(err);
-            }
+			res.send(user);
+		});
+	},
+	createUser(req, res) {
+		const newUser = new User(req.body);
+		newUser.save((err, user) => {
+			if (err) 
+				res.json(err);
+      
 
-            mailHelper.sendEmailVerify(req.body.contact.email);
-            res.send(user);
-        });
-    },
+			mailHelper.sendEmailVerify(req.body.contact.email);
+			res.send(user);
+		});
+	},
 
-    getUser(req, res){
-        User.findById(req.params.id, function(err, user){
-            if(err){
-                res.json(err);
-            }
+	getUser(req, res) {
+		User.findById(req.params.id, (err, user) => {
+			if (err) 
+				res.json(err);
+      
 
-            res.send(user);
-        });
-    },
+			res.send(user);
+		});
+	},
 
-    updateUser(req, res){
-        User.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, user){
-            if (err){
-                res.json(err);
-            }
+	updateUser(req, res) {
+		User.findOneAndUpdate(
+			{ _id: req.params.id },
+			req.body,
+			{ new: true },
+			(err, user) => {
+				if (err) 
+					res.json(err);
+        
 
-            res.json({ message: "user updated", user });
-        });
-    },
-    deleteUser(req, res){
-        User.remove({_id: req.params.id}, function(err, user){
-            if(err){
-                res.json(err);
-            }
+				res.json({ message: 'user updated', user });
+			});
+	},
+	deleteUser(req, res) {
+		User.remove({ _id: req.params.id }, (err, user) => {
+			if (err) 
+				res.json(err);
+      
 
-            res.json({user});
-        });
-    },
-    loginUser(req, res){
-        User.find({"contact.email": req.body.email, password: req.body.password}, function (err, user) {
-            if (err){
-                res.json(err);
-            }
+			res.json({ user });
+		});
+	},
+	loginUser(req, res) {
+		User.find(
+			{ 'contact.email': req.body.email, password: req.body.password },
+			(err, user) => {
+				if (err) 
+					res.json(err);
+        
 
-            if(user.length === 0){
-                res.sendStatus(404);
-            }
-            else{
-                res.send(user);
-            }
-        });
-    },
+				if (user.length === 0) 
+					res.sendStatus(404);
+				else 
+					res.send(user);
+        
+			});
+	},
 
-    verifyUser(req,res) {
-        User.findOneAndUpdate({"contact.email": req.params.email}, {isVerified: true}, function(err, user){
-            if (err){
-                res.json(err);
-            }
+	verifyUser(req, res) {
+		User.findOneAndUpdate(
+			{ 'contact.email': req.params.email },
+			{ isVerified: true },
+			(err, user) => {
+				if (err) 
+					res.json(err);
+        
 
-            res.send("account verified");
-        });
-    },
+				res.send('account verified');
+			});
+	},
 
-    getCords(req,res) { 
-        googleMapsClient.createClient({
-            key: process.env.GMAPS
-        });
+	getCords(req, res) {
+		googleMapsClient.createClient({
+			key: process.env.GMAPS,
+		});
 
-        googleMapsClient.geocode({
-            address: req.body.address
-        }, function(err, response) {
-            if (!err) {
-                res.send(response.json.results);
-            } else {
-                res.send(404);
-            }
-        });
-
-    } 
-
+		googleMapsClient.geocode(
+			{
+				address: req.body.address,
+			},
+			(err, response) => {
+				if (!err) 
+					res.send(response.json.results);
+				else 
+					res.send(404);
+        
+			});
+	}
 };
