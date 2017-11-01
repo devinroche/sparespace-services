c487c06d06ed704b031aa7e209ab02085ebb78de
devin <roche.devin@ymail.com>
2017-10-31 22:43:38 -0700

    add travisci

--- models/userModel.js
+++ models/userModel.js
@@ -1,20 +1,23 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
 const Schema = mongoose.Schema;
 
-var user = new Schema ({
-    fullname: { type: String, required: true }, 
-    password: { type: String, required: true }, 
-    contact: {
-        email: { type: String, required: true, unique: true},
-        phone: {type:String, required: true }, 
-        address: String
-    },
-    userType: {type: String, required:true, default: "renter" },
-    isRenting: { type: Boolean, default: false },
-    isHosting: { type: Boolean, default: false },
-    isVerified: {type: Boolean,default: false},
-}, { collection: "sparespaceusers" });
+const user = new Schema(
+	{
+		fullname: { type: String, required: true },
+		password: { type: String, required: true },
+		contact: {
+			email: { type: String, required: true, unique: true },
+			phone: { type: String, required: true },
+			address: String,
+		},
+		userType: { type: String, required: true, default: 'renter' },
+		isRenting: { type: Boolean, default: false },
+		isHosting: { type: Boolean, default: false },
+		isVerified: { type: Boolean, default: false },
+	},{ collection: 'sparespaceusers' }
+);
 
 user.plugin(uniqueValidator);
-module.exports = mongoose.model("User", user);
+module.exports = mongoose.model('User', user);
--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,28 @@
-const helpers = require("../controllers/userHelper");
-const listHelper = require("../controllers/listingHelper");
+const helpers = require('../controllers/userHelper');
+const listHelper = require('../controllers/listingHelper');
 
-module.exports = function(app) {
-    app.route("/users")
-        .get(helpers.allUsers)
-        .post(helpers.createUser);
+module.exports = function (app) {
+	app
+		.route('/users')
+		.get(helpers.allUsers)
+		.post(helpers.createUser);
 
-    app.route("/user/:id")
-        .get(helpers.getUser)
-        .put(helpers.updateUser)
-        .delete(helpers.deleteUser);
+	app
+		.route('/user/:id')
+		.get(helpers.getUser)
+		.put(helpers.updateUser)
+		.delete(helpers.deleteUser);
 
-    app.route("/login")
-        .post(helpers.loginUser);
+	app.route('/login').post(helpers.loginUser);
 
-    app.route("/verify/:email")
-        .get(helpers.verifyUser);
+	app.route('/verify/:email').get(helpers.verifyUser);
 
-    app.route("/marker")
-        .post(helpers.getCords);
+	app.route('/marker').post(helpers.getCords);
 
-    app.route("/u/listing")
-        .get(listHelper.allListings)
-        .post(listHelper.newListing);
+	app
+		.route('/u/listing')
+		.get(listHelper.allListings)
+		.post(listHelper.newListing);
 
-    app.route("/u/listing/:id")
-        .get(listHelper.listingDetails);
+	app.route('/u/listing/:id').get(listHelper.listingDetails);
 };
