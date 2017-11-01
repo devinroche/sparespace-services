a246b6732ff9bc91f28dea5657492455820eea2c
devin <roche.devin@ymail.com>
2017-10-31 22:43:38 -0700

    add travisci

--- controllers/mailHelper.js
+++ controllers/mailHelper.js
@@ -1,25 +1,25 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 module.exports = {
-    sendEmailVerify(email) {  
-        var smtpTransport = nodemailer.createTransport({
-            service: "gmail",
-            host: "smtp.gmail.com",
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASS
-            }
-        });
+	sendEmailVerify(email) {
+		const smtpTransport = nodemailer.createTransport({
+			service: 'gmail',
+			host: 'smtp.gmail.com',
+			auth: {
+				user: process.env.EMAIL,
+				pass: process.env.PASS,
+			},
+		});
 
-        var mailOptions={
-            to : email,
-            subject : "Sparespace Verification",
-            text: "fart",
-            html : "<p>Click <a href='http://localhost:3001/verify/" + email + "'>Here</a> to verify your account</p>"
-        };
-        
-        smtpTransport.sendMail(mailOptions, function(error, response){
-            smtpTransport.close();
-        });
-    },
+		const mailOptions = {
+			to: email,
+			subject: 'Sparespace Verification',
+			text: 'fart',
+			html: `<p>Click <a href='http://localhost:3001/verify/${email}'>Here</a> to verify your account</p>`,
+		};
+
+		smtpTransport.sendMail(mailOptions, (error, response) => {
+			smtpTransport.close();
+		});
+	},
 };
