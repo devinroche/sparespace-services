1db46ee1c13155939c24625cd71d19d974cdfa72
devin <roche.devin@ymail.com>
2017-11-09 15:04:09 -0800

    replace duration with price for listing

--- models/listingModel.js
+++ models/listingModel.js
@@ -6,7 +6,7 @@ const listing = new Schema(
 	{
 		_host: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 		title: { type: String, required: true },
-		duration: { type: String, required: true },
+		price: { type: String, required: true },
 		description: { type: String, required: true },
 		images: [String]
 	},{ collection: 'sparespacelisting' }
