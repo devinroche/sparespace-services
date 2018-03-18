df1c5e6da88b40e9a1d41a0958fa8a06b4f18e32
Devin Roche <Roche.devin@ymail.com>
2018-03-18 14:57:21 -0700

    remove leading text

--- controllers/userHelper.js
+++ controllers/userHelper.js
@@ -103,7 +103,7 @@ module.exports = {
 			if (err) 
 				return res.json(err);
 
-			res.cookie('v', true, { maxAge: 900000, secure: false, httpOnly: false, overwrite: true, domain: 'https://inspiring-goldstine-bc424a.netlify.com/'});
+			res.cookie('v', true, { maxAge: 900000, secure: false, httpOnly: false, overwrite: true, domain: '.inspiring-goldstine-bc424a.netlify.com'});
 			return res.redirect('https://inspiring-goldstine-bc424a.netlify.com/')
 		});
 	},
