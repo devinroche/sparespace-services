36e5c4df1acb49b7831689cbead50b39519dff3d
Devin Roche <Roche.devin@ymail.com>
2018-04-18 13:32:37 -0700

    change all to use

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ const app = express();
 app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.all('*', function (req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
