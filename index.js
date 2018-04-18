91dfa52d5ad206882a82930925cbbe73031e8af1
Devin Roche <Roche.devin@ymail.com>
2018-04-18 13:29:41 -0700

    test new cors function

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use(function (req, res, next) {
+app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
 
