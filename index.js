d956ee321c573dc39d22274e5592dec84529c1e8
Devin Roche <Roche.devin@ymail.com>
2018-04-18 13:35:03 -0700

    add more fields to cors

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
     next();
 });
 
